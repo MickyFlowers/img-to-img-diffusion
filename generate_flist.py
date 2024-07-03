08a2f43095d22c062c539538b1734a700119c180
cyx <2679417814@qq.com>
2024-07-03 10:56:38 +0800

    add vs dataset

---
+++ generate_flist.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+data_root = "../data/ddpm_visual_servo/img"
+flist = []
+fo = open(os.path.join(data_root, 'train.flist'), 'w')
+for i in range(100000):
+    img = "img-{}".format(i)
+    fo.write(img + "\n")
