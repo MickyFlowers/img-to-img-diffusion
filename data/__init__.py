f91660aeb52a7471dbaa0f1f64520c6d1916ceb9
cyx <2679417814@qq.com>
2024-07-03 10:59:30 +0800

    fixed

--- data/__init__.py
+++ data/__init__.py
@@ -10,62 +10,87 @@ from core.praser import init_obj
 
 
 def define_dataloader(logger, opt):
-    """ create train/test dataloader and validation dataloader,  validation dataloader is None when phase is test or not GPU 0 """
-    '''create dataset and set random seed'''
-    dataloader_args = opt['datasets'][opt['phase']]['dataloader']['args']
-    worker_init_fn = partial(Util.set_seed, gl_seed=opt['seed'])
+    """create train/test dataloader and validation dataloader,  validation dataloader is None when phase is test or not GPU 0"""
+    """create dataset and set random seed"""
+    dataloader_args = opt["datasets"][opt["phase"]]["dataloader"]["args"]
+    worker_init_fn = partial(Util.set_seed, gl_seed=opt["seed"])
 
     phase_dataset, val_dataset = define_dataset(logger, opt)
 
-    '''create datasampler'''
+    """create datasampler"""
     data_sampler = None
-    if opt['distributed']:
-        data_sampler = DistributedSampler(phase_dataset, shuffle=dataloader_args.get('shuffle', False), num_replicas=opt['world_size'], rank=opt['global_rank'])
-        dataloader_args.update({'shuffle':False}) # sampler option is mutually exclusive with shuffle 
-    
-    ''' create dataloader and validation dataloader '''
-    dataloader = DataLoader(phase_dataset, sampler=data_sampler, worker_init_fn=worker_init_fn, **dataloader_args)
-    ''' val_dataloader don't use DistributedSampler to run only GPU 0! '''
-    if opt['global_rank']==0 and val_dataset is not None:
-        dataloader_args.update(opt['datasets'][opt['phase']]['dataloader'].get('val_args',{}))
-        val_dataloader = DataLoader(val_dataset, worker_init_fn=worker_init_fn, **dataloader_args) 
+    if opt["distributed"]:
+        data_sampler = DistributedSampler(
+            phase_dataset,
+            shuffle=dataloader_args.get("shuffle", False),
+            num_replicas=opt["world_size"],
+            rank=opt["global_rank"],
+        )
+        dataloader_args.update(
+            {"shuffle": False}
+        )  # sampler option is mutually exclusive with shuffle
+
+    """ create dataloader and validation dataloader """
+    dataloader = DataLoader(
+        phase_dataset,
+        sampler=data_sampler,
+        worker_init_fn=worker_init_fn,
+        **dataloader_args
+    )
+    """ val_dataloader don't use DistributedSampler to run only GPU 0! """
+    if opt["global_rank"] == 0 and val_dataset is not None:
+        dataloader_args.update(
+            opt["datasets"][opt["phase"]]["dataloader"].get("val_args", {})
+        )
+        val_dataloader = DataLoader(
+            val_dataset, worker_init_fn=worker_init_fn, **dataloader_args
+        )
     else:
         val_dataloader = None
     return dataloader, val_dataloader
 
 
 def define_dataset(logger, opt):
-    ''' loading Dataset() class from given file's name '''
-    dataset_opt = opt['datasets'][opt['phase']]['which_dataset']
-    phase_dataset = init_obj(dataset_opt, logger, default_file_name='data.dataset', init_type='Dataset')
+    """loading Dataset() class from given file's name"""
+    dataset_opt = opt["datasets"][opt["phase"]]["which_dataset"]
+    phase_dataset = init_obj(
+        dataset_opt, logger, default_file_name="data.dataset", init_type="Dataset"
+    )
     val_dataset = None
 
     valid_len = 0
     data_len = len(phase_dataset)
-    if 'debug' in opt['name']:
-        debug_split = opt['debug'].get('debug_split', 1.0)
+    if "debug" in opt["name"]:
+        debug_split = opt["debug"].get("debug_split", 1.0)
         if isinstance(debug_split, int):
             data_len = debug_split
         else:
             data_len *= debug_split
 
-    dataloder_opt = opt['datasets'][opt['phase']]['dataloader']
-    valid_split = dataloder_opt.get('validation_split', 0)    
-    
-    ''' divide validation dataset, valid_split==0 when phase is test or validation_split is 0. '''
-    if valid_split > 0.0 or 'debug' in opt['name']: 
+    dataloder_opt = opt["datasets"][opt["phase"]]["dataloader"]
+    valid_split = dataloder_opt.get("validation_split", 0)
+
+    """ divide validation dataset, valid_split==0 when phase is test or validation_split is 0. """
+    if valid_split > 0.0 or "debug" in opt["name"]:
         if isinstance(valid_split, int):
-            assert valid_split < data_len, "Validation set size is configured to be larger than entire dataset."
+            assert (
+                valid_split < data_len
+            ), "Validation set size is configured to be larger than entire dataset."
             valid_len = valid_split
         else:
             valid_len = int(data_len * valid_split)
         data_len -= valid_len
-        phase_dataset, val_dataset = subset_split(dataset=phase_dataset, lengths=[data_len, valid_len], generator=Generator().manual_seed(opt['seed']))
-    
-    logger.info('Dataset for {} have {} samples.'.format(opt['phase'], data_len))
-    if opt['phase'] == 'train':
-        logger.info('Dataset for {} have {} samples.'.format('val', valid_len))   
+        phase_dataset, val_dataset = subset_split(
+            dataset=phase_dataset,
+            lengths=[data_len, valid_len],
+            generator=Generator().manual_seed(opt["seed"]),
+        )
+
+    logger.info("Dataset for {} have {} samples.".format(opt["phase"], data_len))
+    if opt["phase"] == "train":
+        logger.info("Dataset for {} have {} samples.".format("val", valid_len))
     return phase_dataset, val_dataset
+
 
 def subset_split(dataset, lengths, generator):
     """
--- data/dataset.py
+++ data/dataset.py
@@ -5,22 +5,38 @@ import os
 import torch
 import numpy as np
 
-from .util.mask import (bbox2mask, brush_stroke_mask, get_irregular_mask, random_bbox, random_cropping_bbox)
+from .util.mask import (
+    bbox2mask,
+    brush_stroke_mask,
+    get_irregular_mask,
+    random_bbox,
+    random_cropping_bbox,
+)
 
 IMG_EXTENSIONS = [
-    '.jpg', '.JPG', '.jpeg', '.JPEG',
-    '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
+    ".jpg",
+    ".JPG",
+    ".jpeg",
+    ".JPEG",
+    ".png",
+    ".PNG",
+    ".ppm",
+    ".PPM",
+    ".bmp",
+    ".BMP",
 ]
+
 
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in IMG_EXTENSIONS)
 
+
 def make_dataset(dir):
     if os.path.isfile(dir):
-        images = [i for i in np.genfromtxt(dir, dtype=np.str, encoding='utf-8')]
+        images = [i for i in np.genfromtxt(dir, dtype=np.str, encoding="utf-8")]
     else:
         images = []
-        assert os.path.isdir(dir), '%s is not a valid directory' % dir
+        assert os.path.isdir(dir), "%s is not a valid directory" % dir
         for root, _, fnames in sorted(os.walk(dir)):
             for fname in sorted(fnames):
                 if is_image_file(fname):
@@ -29,24 +45,35 @@ def make_dataset(dir):
 
     return images
 
+
 def pil_loader(path):
-    return Image.open(path).convert('RGB')
+    return Image.open(path).convert("RGB")
+
 
 class InpaintDataset(data.Dataset):
-    def __init__(self, data_root, mask_config={}, data_len=-1, image_size=[256, 256], loader=pil_loader):
+    def __init__(
+        self,
+        data_root,
+        mask_config={},
+        data_len=-1,
+        image_size=[256, 256],
+        loader=pil_loader,
+    ):
         imgs = make_dataset(data_root)
         if data_len > 0:
-            self.imgs = imgs[:int(data_len)]
+            self.imgs = imgs[: int(data_len)]
         else:
             self.imgs = imgs
-        self.tfs = transforms.Compose([
+        self.tfs = transforms.Compose(
+            [
                 transforms.Resize((image_size[0], image_size[1])),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
-        ])
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
         self.loader = loader
         self.mask_config = mask_config
-        self.mask_mode = self.mask_config['mask_mode']
+        self.mask_mode = self.mask_config["mask_mode"]
         self.image_size = image_size
 
     def __getitem__(self, index):
@@ -54,56 +81,68 @@ class InpaintDataset(data.Dataset):
         path = self.imgs[index]
         img = self.tfs(self.loader(path))
         mask = self.get_mask()
-        cond_image = img*(1. - mask) + mask*torch.randn_like(img)
-        mask_img = img*(1. - mask) + mask
+        cond_image = img * (1.0 - mask) + mask * torch.randn_like(img)
+        mask_img = img * (1.0 - mask) + mask
 
-        ret['gt_image'] = img
-        ret['cond_image'] = cond_image
-        ret['mask_image'] = mask_img
-        ret['mask'] = mask
-        ret['path'] = path.rsplit("/")[-1].rsplit("\\")[-1]
+        ret["gt_image"] = img
+        ret["cond_image"] = cond_image
+        ret["mask_image"] = mask_img
+        ret["mask"] = mask
+        ret["path"] = path.rsplit("/")[-1].rsplit("\\")[-1]
         return ret
 
     def __len__(self):
         return len(self.imgs)
 
     def get_mask(self):
-        if self.mask_mode == 'bbox':
+        if self.mask_mode == "bbox":
             mask = bbox2mask(self.image_size, random_bbox())
-        elif self.mask_mode == 'center':
+        elif self.mask_mode == "center":
             h, w = self.image_size
-            mask = bbox2mask(self.image_size, (h//4, w//4, h//2, w//2))
-        elif self.mask_mode == 'irregular':
+            mask = bbox2mask(self.image_size, (h // 4, w // 4, h // 2, w // 2))
+        elif self.mask_mode == "irregular":
             mask = get_irregular_mask(self.image_size)
-        elif self.mask_mode == 'free_form':
+        elif self.mask_mode == "free_form":
             mask = brush_stroke_mask(self.image_size)
-        elif self.mask_mode == 'hybrid':
+        elif self.mask_mode == "hybrid":
             regular_mask = bbox2mask(self.image_size, random_bbox())
-            irregular_mask = brush_stroke_mask(self.image_size, )
+            irregular_mask = brush_stroke_mask(
+                self.image_size,
+            )
             mask = regular_mask | irregular_mask
-        elif self.mask_mode == 'file':
+        elif self.mask_mode == "file":
             pass
         else:
             raise NotImplementedError(
-                f'Mask mode {self.mask_mode} has not been implemented.')
-        return torch.from_numpy(mask).permute(2,0,1)
+                f"Mask mode {self.mask_mode} has not been implemented."
+            )
+        return torch.from_numpy(mask).permute(2, 0, 1)
 
 
 class UncroppingDataset(data.Dataset):
-    def __init__(self, data_root, mask_config={}, data_len=-1, image_size=[256, 256], loader=pil_loader):
+    def __init__(
+        self,
+        data_root,
+        mask_config={},
+        data_len=-1,
+        image_size=[256, 256],
+        loader=pil_loader,
+    ):
         imgs = make_dataset(data_root)
         if data_len > 0:
-            self.imgs = imgs[:int(data_len)]
+            self.imgs = imgs[: int(data_len)]
         else:
             self.imgs = imgs
-        self.tfs = transforms.Compose([
+        self.tfs = transforms.Compose(
+            [
                 transforms.Resize((image_size[0], image_size[1])),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
-        ])
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
         self.loader = loader
         self.mask_config = mask_config
-        self.mask_mode = self.mask_config['mask_mode']
+        self.mask_mode = self.mask_config["mask_mode"]
         self.image_size = image_size
 
     def __getitem__(self, index):
@@ -111,95 +150,127 @@ class UncroppingDataset(data.Dataset):
         path = self.imgs[index]
         img = self.tfs(self.loader(path))
         mask = self.get_mask()
-        cond_image = img*(1. - mask) + mask*torch.randn_like(img)
-        mask_img = img*(1. - mask) + mask
+        cond_image = img * (1.0 - mask) + mask * torch.randn_like(img)
+        mask_img = img * (1.0 - mask) + mask
 
-        ret['gt_image'] = img
-        ret['cond_image'] = cond_image
-        ret['mask_image'] = mask_img
-        ret['mask'] = mask
-        ret['path'] = path.rsplit("/")[-1].rsplit("\\")[-1]
+        ret["gt_image"] = img
+        ret["cond_image"] = cond_image
+        ret["mask_image"] = mask_img
+        ret["mask"] = mask
+        ret["path"] = path.rsplit("/")[-1].rsplit("\\")[-1]
         return ret
 
     def __len__(self):
         return len(self.imgs)
 
     def get_mask(self):
-        if self.mask_mode == 'manual':
-            mask = bbox2mask(self.image_size, self.mask_config['shape'])
-        elif self.mask_mode == 'fourdirection' or self.mask_mode == 'onedirection':
-            mask = bbox2mask(self.image_size, random_cropping_bbox(mask_mode=self.mask_mode))
-        elif self.mask_mode == 'hybrid':
-            if np.random.randint(0,2)<1:
-                mask = bbox2mask(self.image_size, random_cropping_bbox(mask_mode='onedirection'))
+        if self.mask_mode == "manual":
+            mask = bbox2mask(self.image_size, self.mask_config["shape"])
+        elif self.mask_mode == "fourdirection" or self.mask_mode == "onedirection":
+            mask = bbox2mask(
+                self.image_size, random_cropping_bbox(mask_mode=self.mask_mode)
+            )
+        elif self.mask_mode == "hybrid":
+            if np.random.randint(0, 2) < 1:
+                mask = bbox2mask(
+                    self.image_size, random_cropping_bbox(mask_mode="onedirection")
+                )
             else:
-                mask = bbox2mask(self.image_size, random_cropping_bbox(mask_mode='fourdirection'))
-        elif self.mask_mode == 'file':
+                mask = bbox2mask(
+                    self.image_size, random_cropping_bbox(mask_mode="fourdirection")
+                )
+        elif self.mask_mode == "file":
             pass
         else:
             raise NotImplementedError(
-                f'Mask mode {self.mask_mode} has not been implemented.')
-        return torch.from_numpy(mask).permute(2,0,1)
-    
+                f"Mask mode {self.mask_mode} has not been implemented."
+            )
+        return torch.from_numpy(mask).permute(2, 0, 1)
+
+
 class VSDataset(data.Dataset):
-    def __init__(self, data_root, data_flist, data_len=-1, image_size=[224, 224], loader=pil_loader):
+    def __init__(
+        self,
+        data_root,
+        data_flist,
+        data_len=-1,
+        image_size=[224, 224],
+        loader=pil_loader,
+    ):
         self.data_root = data_root
         flist = make_dataset(data_flist)
         if data_len > 0:
-            self.flist = flist[:int(data_len)]
+            self.flist = flist[: int(data_len)]
         else:
             self.flist = flist
-        self.tfs = transforms.Compose([
+        self.tfs = transforms.Compose(
+            [
                 transforms.Resize((image_size[0], image_size[1])),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
-        ])
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
         self.loader = loader
         self.image_size = image_size
 
     def __getitem__(self, index):
         ret = {}
-        file_name = str(self.flist[index]) + '.png'
+        file_name = str(self.flist[index]) + ".png"
 
-        img = self.tfs(self.loader('{}/{}/{}'.format(self.data_root, 'img', file_name)))
-        cond_image = self.tfs(self.loader('{}/{}/{}'.format(self.data_root, 'seg', file_name)))
+        img = self.tfs(self.loader("{}/{}/{}".format(self.data_root, "img", file_name)))
+        cond_image = self.tfs(
+            self.loader("{}/{}/{}".format(self.data_root, "seg", file_name))
+        )
 
-        ret['gt_image'] = img
-        ret['cond_image'] = cond_image
-        ret['path'] = file_name
+        ret["gt_image"] = img
+        ret["cond_image"] = cond_image
+        ret["path"] = file_name
         return ret
 
     def __len__(self):
         return len(self.flist)
+
+
 class ColorizationDataset(data.Dataset):
-    def __init__(self, data_root, data_flist, data_len=-1, image_size=[224, 224], loader=pil_loader):
+    def __init__(
+        self,
+        data_root,
+        data_flist,
+        data_len=-1,
+        image_size=[224, 224],
+        loader=pil_loader,
+    ):
         self.data_root = data_root
         flist = make_dataset(data_flist)
         if data_len > 0:
-            self.flist = flist[:int(data_len)]
+            self.flist = flist[: int(data_len)]
         else:
             self.flist = flist
-        self.tfs = transforms.Compose([
+        self.tfs = transforms.Compose(
+            [
                 transforms.Resize((image_size[0], image_size[1])),
                 transforms.ToTensor(),
-                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5,0.5, 0.5])
-        ])
+                transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5]),
+            ]
+        )
         self.loader = loader
         self.image_size = image_size
 
     def __getitem__(self, index):
         ret = {}
-        file_name = str(self.flist[index]).zfill(5) + '.png'
+        file_name = str(self.flist[index]).zfill(5) + ".png"
 
-        img = self.tfs(self.loader('{}/{}/{}'.format(self.data_root, 'color', file_name)))
-        cond_image = self.tfs(self.loader('{}/{}/{}'.format(self.data_root, 'gray', file_name)))
+        img = self.tfs(
+            self.loader("{}/{}/{}".format(self.data_root, "color", file_name))
+        )
+        cond_image = self.tfs(
+            self.loader("{}/{}/{}".format(self.data_root, "gray", file_name))
+        )
 
-        ret['gt_image'] = img
-        ret['cond_image'] = cond_image
-        ret['path'] = file_name
+        ret["gt_image"] = img
+        ret["cond_image"] = cond_image
+        ret["path"] = file_name
         return ret
 
     def __len__(self):
         return len(self.flist)
-
-
